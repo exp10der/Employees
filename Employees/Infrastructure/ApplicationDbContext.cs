011846998c7fd070945a4e8d6b897b2c95049bf0
Konstantin <konstantin.writing.code@gmail.com>
2016-11-25 10:50:42 +0300

    (feature) Configure DbContext

--- Employees/Infrastructure/ApplicationDbContext.cs
+++ Employees/Infrastructure/ApplicationDbContext.cs
@@ -5,10 +5,20 @@
 
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext()
+        {
+        }
+
         public ApplicationDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
         {
         }
 
         public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Ignore(e => e.FullName);
+        }
     }
 }
