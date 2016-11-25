35566545b24f816447124834560282c6fd22b44f
Konstantin <konstantin.writing.code@gmail.com>
2016-11-25 10:28:42 +0300

    (new) Create Employee

---
+++ Employees/Domain/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Employees.Domain
+{
+    public class Employee
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
