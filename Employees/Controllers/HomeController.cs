33032288551b60e77f90c814ea0786ab4798a6b4
Konstantin <konstantin.writing.code@gmail.com>
2016-11-25 17:38:20 +0300

    (-) Remove entity test

--- Employees/Controllers/HomeController.cs
+++ Employees/Controllers/HomeController.cs
@@ -1,41 +1,20 @@
 ﻿namespace Employees.Controllers
 {
-    using System.Diagnostics;
-    using System.Linq;
     using System.Web.Mvc;
-    using DelegateDecompiler;
     using Infrastructure;
 
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly IFooDefaultConventions _fooDefaultConventions;
 
-        public HomeController(IFooDefaultConventions fooDefaultConventions, ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context)
         {
-            _fooDefaultConventions = fooDefaultConventions;
             _context = context;
-
-            var test = _context.Employees.Decompile().ToList();
         }
 
         public ActionResult Index()
         {
-            Debug.Print(_fooDefaultConventions.Foo());
             return View();
-        }
-    }
-
-    public interface IFooDefaultConventions
-    {
-        string Foo();
-    }
-
-    public class FooDefaultConventions : IFooDefaultConventions
-    {
-        public string Foo()
-        {
-            return "Foo";
         }
     }
 }
