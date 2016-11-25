4a49a557cf2db8ec9355dc4b586725f9d94d917b
Konstantin <konstantin.writing.code@gmail.com>
2016-11-25 10:39:32 +0300

    (new) Create DbContext

---
+++ Employees/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Employees.Infrastructure
+{
+    using System.Data.Entity;
+    using Domain;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
