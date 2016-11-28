acff74c5a707022d7e9f9ae18fd7f63bb4dea39f
Konstantin <konstantin.writing.code@gmail.com>
2016-11-28 09:57:41 +0300

    (new) Install package FluentValidation, FluentValidation.MVC5

---
+++ Employees/Infrastructure/DependencyResolution/StructureMapValidatorFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Employees.Infrastructure.DependencyResolution
+{
+    using System;
+    using FluentValidation;
+    using StructureMap;
+
+    public class StructureMapValidatorFactory : ValidatorFactoryBase
+    {
+        public IContainer Container
+        {
+            get { throw new NotImplementedException("Need add Property Injection"); }
+            set { throw new NotImplementedException("Need add Property Injection"); }
+        }
+
+        public override IValidator CreateInstance(Type validatorType)
+        {
+            return Container.TryGetInstance(validatorType) as IValidator;
+        }
+    }
+}
