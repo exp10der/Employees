8053dd4b115e75121e3a8294c4e22ef3376e5205
Konstantin <konstantin.writing.code@gmail.com>
2016-11-29 07:51:49 +0300

    (new) Add Case TransactionFilter

---
+++ Employees/Infrastructure/Data/TransactionFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace Employees.Infrastructure.Data
+{
+    using System.Web.Mvc;
+
+    public class TransactionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // BeginTransaction
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            // CloseTransaction
+        }
+    }
+}
