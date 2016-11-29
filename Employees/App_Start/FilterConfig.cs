4b1b67457e83739ce069ef519f2017ddfa7ae0cc
Konstantin <konstantin.writing.code@gmail.com>
2016-11-29 07:52:37 +0300

    (new) Register global filter TransactionFilter

---
+++ Employees/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Employees
+{
+    using System.Web.Mvc;
+    using Infrastructure.Data;
+
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new TransactionFilter());
+        }
+    }
+}
