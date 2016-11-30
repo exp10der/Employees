e5a2a65fac3122fc8a49c4cb090ae786e160eb4e
Konstantin <konstantin.writing.code@gmail.com>
2016-11-30 22:13:35 +0300

    (feature) Decorate FilterAttributeFilterProvider for setter injection with StructureMap

---
+++ Employees/Infrastructure/DependencyResolution/FilterProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace Employees.Infrastructure.DependencyResolution
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using StructureMap;
+
+    public class FilterProvider : FilterAttributeFilterProvider
+    {
+        private readonly Func<IContainer> _container;
+
+        public FilterProvider(Func<IContainer> container)
+        {
+            _container = container;
+        }
+
+        public override IEnumerable<Filter> GetFilters(ControllerContext controllerContext,
+            ActionDescriptor actionDescriptor)
+        {
+            var filters = base.GetFilters(controllerContext, actionDescriptor);
+
+            var container = _container();
+
+            foreach (var filter in filters)
+            {
+                container.BuildUp(filter.Instance);
+                yield return filter;
+            }
+        }
+    }
+}
