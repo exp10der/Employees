5e34a8a122c41d3cfc1ea3cfedc47bff66b2f831
Konstantin <konstantin.writing.code@gmail.com>
2016-11-30 22:47:03 +0300

    (new) Add property db context for Transaction

--- Employees/Infrastructure/Data/TransactionFilter.cs
+++ Employees/Infrastructure/Data/TransactionFilter.cs
@@ -4,6 +4,8 @@
 
     public class TransactionFilter : ActionFilterAttribute
     {
+        public ApplicationDbContext Context { get; set; }
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             // BeginTransaction
